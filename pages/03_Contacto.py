c14f4f2425d7ab3c483975b57ccfc5d1bd05a52e
mberriosmoreno <94083004+mberriosmoreno@users.noreply.github.com>
2025-01-29 08:16:04 -0600

    Create 03_Contacto.py

---
+++ pages/03_Contacto.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("📢 Conéctate con nosotros")
+
+st.markdown("[Facebook](https://www.facebook.com)")
+st.markdown("[Twitter](https://www.twitter.com)")
+st.markdown("[Instagram](https://www.instagram.com)")
